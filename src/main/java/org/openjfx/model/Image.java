64a7ca588650c1b1261016fb4e4c812bfd4c84c0
Carlos Eduardo Nogueira <cen.silva@unesp.br>
2024-05-16 18:11:16 -0300

    Request no LoginController feito

---
+++ src/main/java/org/openjfx/model/Image.java
@@ -0,0 +1,37 @@
+package org.openjfx.model;
+
+import java.io.InputStream;
+import com.google.gson.annotations.SerializedName;
+
+public class Image {
+  @SerializedName("id")
+  private int id;
+
+  @SerializedName("image")
+  private Byte[] image;
+
+  public Image() {}
+
+  public Image(int id) {
+    this.id = id;
+  }
+  public Image(int id, Byte[] image) {
+    this.id = id;
+    this.image = image;
+  }
+
+  public int getId() {
+    return id;
+  }
+  public Byte[] getImage() {
+    return image;
+  }
+
+  public void setId(int id) { 
+    this.id = id;
+  }
+
+  public void setImage(Byte[] image) {
+    this.image = image;
+  }
+}
---
+++ src/main/java/org/openjfx/model/User.java
@@ -0,0 +1,104 @@
+package org.openjfx.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+  @SerializedName("id")
+  private int id;       
+
+  @SerializedName("name")
+  private String name;   
+  
+  @SerializedName("username")
+  private String username;
+
+  @SerializedName("email")
+  private String email;
+
+  @SerializedName("password")
+  private String password; 
+
+  @SerializedName("profilePic")
+  private Image profilePic;
+
+  @SerializedName("auth")
+  private boolean auth;
+
+  public User() {
+  }
+
+  public User(int id, String name, String username, String email, String password, Image profilePic) {
+    this.id = id;
+    this.name = name;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.profilePic = profilePic;
+    this.auth = false;
+  }
+
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setUsername(String userName) {
+    this.username = userName;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setProfilePic(Image pfp) {
+    this.profilePic = pfp;
+  }
+
+  public void setAuth(boolean auth) {
+    this.auth = auth;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Image getProfilePic() {
+    return profilePic;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUsername(){
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+
+  public String toString() {
+    return "User{" +
+    "id=" + id + 
+    ", username='" + username + '\'' +
+    ", name='" + name + '\'' +
+    ", email='" + email + '\'' +
+    ", password='" + password + '\'' +
+    ", pfp= '" +profilePic + '\'' +
+    ", auth= '" +auth+ '\'' +
+    '}';
+  }}
