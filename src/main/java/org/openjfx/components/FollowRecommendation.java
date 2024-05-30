09f721aa92f496d8709837bb38446151b2cc2d37
CEN-s <cen.silva@unesp.br>
2024-05-30 10:15:06 -0300

    Fiz umas coisas

---
+++ src/main/java/org/openjfx/components/FollowRecommendation.java
@@ -0,0 +1,18 @@
+package org.openjfx.components;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+
+public class UserRecommendationComponent extends FlowPane {
+    @FXML
+    FlowPane followPane;
+    @FXML
+    ImageView profilePic;
+    @FXML
+    Label usernameLabel;
+    @FXML
+    
+
+}
